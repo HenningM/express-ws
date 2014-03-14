59c90c2197011874334f724267700125bec7bf32
Henning Morud <h.morud@sportradar.com>
2014-03-14 15:57:20 +0100

    Initial framework

---
+++ examples/simple.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var expressWs = require('../')(app, server);
+
+app.use(function (req, res, next) {
+  console.log('middleware');
+  req.testing = 'testing';
+  return next();
+});
+
+app.get('/', function(req, res, next){
+  console.log('get route', req.testing);
+  res.end();
+});
+
+app.ws('/', function(ws, req) {
+  ws.on('message', function(msg) {
+    console.log(msg);
+  });
+  console.log('socket', req.testing);
+});
+
+server.listen(3000);
---
+++ index.js
@@ -0,0 +1,44 @@
+var httpMocks = require('node-mocks-http');
+var WebSocketServer = require('ws').Server;
+
+module.exports = function (app, server) {
+  var addSocketRoute = function (route, middleware, callback) {
+    var args = [].splice.call(arguments, 0);
+    if (args.length < 2) {
+      return;
+    } else if (args.length === 2) {
+      middleware = [middleware];
+    } else if (typeof middleware === 'object') {
+      middleware.push(callback);
+    } else {
+      middleware = args.slice(1);
+    }
+
+    var wss = new WebSocketServer({
+      server: server,
+      path: route
+    });
+    wss.on('connection', function(ws) {
+      var dummyRes = httpMocks.createResponse();
+      dummyRes.writeHead = function (statusCode) {
+        if (statusCode > 200) ws.close();
+      };
+      ws.upgradeReq.method = 'ws';
+
+      app.handle(ws.upgradeReq, dummyRes, function(err) {
+        var idx = 0;
+        (function next (err) {
+          if (err) return;
+          var cur = middleware[idx++];
+          if (!middleware[idx]) {
+            cur(ws, ws.upgradeReq);
+          } else {
+            cur(ws.upgradeReq, dummyRes, next);
+          }
+        }());
+      });
+    });
+  };
+
+  app.ws = addSocketRoute;
+};
