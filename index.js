b7924c9d6a4bb900115917c107b3efc9bd778903
Henning Morud <h.morud@sportradar.com>
2015-08-21 14:27:54 +0200

    Add support for regex/dynamic URL parameters

--- index.js
+++ index.js
@@ -4,7 +4,18 @@ var http = require('http');
 var ServerResponse = http.ServerResponse;
 var WebSocketServer = require('ws').Server;
 
-var wsServers = {};
+var wsServer;
+
+var wrapWsHandler = function(handler) {
+    return function(req, res, next) {
+      if (req.ws) {
+        req.wsHandled = true;
+        handler(req.ws, req, next);
+      } else {
+        next();
+      }
+    };
+};
 
 /**
  * @param {express.Application} app
@@ -20,48 +31,36 @@ module.exports = function (app, server) {
     }
   }
 
-  function addSocketRoute(route, middleware, callback) {
-    var args = [].splice.call(arguments, 0);
-    var wsPath = urlJoin(app.mountpath, route);
+  wsServer = new WebSocketServer({ server: server });
 
-    if (args.length < 2)
-      throw new SyntaxError('Invalid number of arguments');
-
-    if (args.length === 2) {
-      middleware = [middleware];
-    } else if (typeof middleware === 'object') {
-      middleware.push(callback);
-    } else {
-      middleware = args.slice(1);
+  wsServer.on('connection', function(ws) {
+    var response = new ServerResponse(ws.upgradeReq);
+    response.writeHead = function (statusCode) {
+      if (statusCode > 200) ws.close();
+    };
+    ws.upgradeReq.ws = ws;
+    if (ws.upgradeReq.url.slice(-1) !== '/') {
+      ws.upgradeReq.url += '/';
     }
+    ws.upgradeReq.url += '.websocket';
 
-    var wss = new WebSocketServer({
-      server: server,
-      path: wsPath
+    app.handle(ws.upgradeReq, response, function() {
+      if (!ws.upgradeReq.wsHandled) {
+        ws.close();
+      }
     });
+  });
 
-    wsServers[wsPath] = wss;
+  function addSocketRoute(route, middleware) {
+    var args = [].splice.call(arguments, 0);
+    if (route.slice(-1) !== '/') {
+      route += '/';
+    }
+    route += '.websocket';
 
-    wss.on('connection', function(ws) {
-      var response = new ServerResponse(ws.upgradeReq);
-      response.writeHead = function (statusCode) {
-        if (statusCode > 200) ws.close();
-      };
-      ws.upgradeReq.method = 'ws';
-
-      app.handle(ws.upgradeReq, response, function(err) {
-        var idx = 0;
-        (function next (err) {
-          if (err) return;
-          var cur = middleware[idx++];
-          if (!middleware[idx]) {
-            cur(ws, ws.upgradeReq);
-          } else {
-            cur(ws.upgradeReq, response, next);
-          }
-        }(err));
-      });
-    });
+    var middlewares = args.slice(1).map(wrapWsHandler);
+    var routeArgs = [route].concat(middlewares);
+    app.get.apply(app, routeArgs);
 
     return app;
   };
@@ -71,7 +70,7 @@ module.exports = function (app, server) {
   return {
     app: app,
     getWss: function (route) {
-      return wsServers[route];
+      return wsServer;
     }
   };
 };
